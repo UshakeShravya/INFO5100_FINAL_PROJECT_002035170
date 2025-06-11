98b0701c9898167ae7088101e21a643a727ce37d
UshakeShravya <ushake.s@northeastern.edu>
2025-06-11 16:37:41 -0400

    Admin Role

---
+++ src/main/java/Business/Role/AdminRole.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.*;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import ui.AdminPanel.AdminDashboardPanel;
+/**
+ *
+ * @author ushakeshravya
+ */
+public class AdminRole extends Role {
+     @Override
+    public JPanel createWorkArea(JPanel workarea, UserAccount account, EcoSystem system) {
+        return new AdminDashboardPanel(workarea, system); // match your constructor
+    }
+    
+}
