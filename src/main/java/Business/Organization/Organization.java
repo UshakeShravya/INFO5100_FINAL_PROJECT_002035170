285e8d2d88c12dc3b10a8064e65e839e344f155e
UshakeShravya <ushake.s@northeastern.edu>
2025-06-15 21:48:33 -0400

    Organization added

---
+++ src/main/java/Business/Organization/Organization.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+/**
+ *
+ * @author ushakeshravya
+ */
+public class Organization {
+    
+    private String name; // make sure this field exists
+
+public String getName() {
+    return name;
+}
+    
+}
---
+++ src/main/java/Business/Organization/OrganizationDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ushakeshravya
+ */
+public class OrganizationDirectory {
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList<>();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+
+    public void addOrganization(Organization org) {
+        organizationList.add(org);
+    }
+    
+}
