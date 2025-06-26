27798040f2c5b55e54bf95c8b3a729b97880276f
UshakeShravya <ushake.s@northeastern.edu>
2025-06-25 23:53:53 -0400

    ConcessionOrganization.java modified

---
+++ src/main/java/Business/Organization/ConcessionOrganization.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import Business.Enterprise.Enterprise;
+
+/**
+ *
+ * @author ushakeshravya
+ */
+public class ConcessionOrganization extends Organization {
+   public ConcessionOrganization() {
+        super("Concession");
+    }
+
+
+ 
+}
