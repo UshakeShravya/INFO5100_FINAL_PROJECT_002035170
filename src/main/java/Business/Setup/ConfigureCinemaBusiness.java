3e37f93ca42efe40c312b2433540a5f63aaf96d8
UshakeShravya <ushake.s@northeastern.edu>
2025-06-25 21:05:43 -0400

    ConfigureCinemaBusiness.java modified

--- src/main/java/Business/Setup/ConfigureCinemaBusiness.java
+++ src/main/java/Business/Setup/ConfigureCinemaBusiness.java
@@ -4,10 +4,28 @@
  */
 package Business.Setup;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.ConcessionOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
 /**
  *
  * @author ushakeshravya
  */
 public class ConfigureCinemaBusiness {
+     public static EcoSystem configure() {
+        EcoSystem system = new EcoSystem();
+
+       Enterprise testEnterprise = new Enterprise("Main Cinema Enterprise"); // create enterprise
+ConcessionOrganization concessionOrg = new ConcessionOrganization(); // create concession org
+testEnterprise.getOrganizationList().add(concessionOrg); // add to enterprise
+system.getEnterpriseList().add(testEnterprise); // register in ecosystem
+
+
+        return system;
+    }
+    
     
 }
