456adefbea1dcb275777d62c9774bc2a6796f5c3
UshakeShravya <ushake.s@northeastern.edu>
2025-06-25 21:01:42 -0400

    Enterprise.java modified

--- src/main/java/Business/Enterprise/Enterprise.java
+++ src/main/java/Business/Enterprise/Enterprise.java
@@ -4,10 +4,37 @@
  */
 package Business.Enterprise;
 
+import Business.Organization.Organization;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  *
  * @author ushakeshravya
  */
 public class Enterprise {
-    
+
+    private String name;
+    private String type; // changed from EnterpriseType to String
+    private List<Organization> organizationList;
+
+    public Enterprise(String name) {
+        this.name = name;
+        this.type = type;
+        this.organizationList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<Organization> getOrganizationList() {
+        return organizationList;
+    }
+
 }
